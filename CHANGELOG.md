1ed8c8f62230930a29a789c2f987d34403e740e3
François Magimel <francois.magimel@ovhcloud.com>
2021-02-24 10:43:07 +0100

    release v1.0.2
    Signed-off-by: François Magimel <francois.magimel@ovhcloud.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # Release notes
 
+## 1.0.2 (2021-02-24)
+
+* Fix(doc): use the right Markdown syntax for links
+* Fix(doc): use the right PyPI link
+* Fix(setup): remove a typo from entry author.
+
+
 ## 1.0.1 (2020-12-23)
 
 Fix: remove a not supported classifier.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="APISpec-fromfile",
-    version="1.0.1",
+    version="1.0.2",
     author="OVHcloud",
     author_email="opensource@ovhcloud.com",
     description="APISpec plugin to import OpenAPI specifications from a file instead of putting YAML into docstrings",
