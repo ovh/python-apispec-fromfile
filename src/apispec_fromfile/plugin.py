1fc31d0b4cbf2bb1a9338e41ef09cc336f858908
zeio <zeionara@gmail.com>
2022-06-24 18:14:27 +0300

    Added live-reload flag for updating spec on every request

--- src/apispec_fromfile/plugin.py
+++ src/apispec_fromfile/plugin.py
@@ -25,26 +25,55 @@ class FromFilePlugin(BasePlugin):
         # get specs
         specs = getattr(view, "__apispec__", None)
 
+        if specs is None:
+            get_specs = getattr(view, "__get_apispec__", None)
+
+            if get_specs is not None:
+                specs = get_specs()
+
         # update operations
         if specs is not None:
             operations.update(specs)
 
 
-def from_file(spec_path):
+def read_spec(path: str):
+    # get the file
+    path = Path(path)
+    if not path.exists():
+        return None
+
+    # get the content
+    return path.read_text()
+
+
+def load_spec(func, path):
+    content = read_spec(path)
+    spec = func.__dict__.get("__previous_apispec__", {})
+
+    if content is None:
+        return spec
+
+    spec.update(load_operations_from_docstring(content))
+    func.___previous_apispec__ = spec
+
+    return spec
+
+
+def from_file(spec_path, live_reload: bool = False):
     """ Decorate an endpoint with an OpenAPI spec file to import. """
 
     def wrapper(func):
-        # get the file
-        path = Path(spec_path)
-        if not path.exists():
+        # save the content in a special attribute of the function
+        content = read_spec(spec_path)
+
+        if content is None:
             return func
 
-        # get the content
-        content = path.read_text()
-
-        # save the content in a special attribute of the function
-        func.__apispec__ = func.__dict__.get("__apispec__", {})
-        func.__apispec__.update(load_operations_from_docstring(content))
+        if live_reload:
+            func.__get_apispec__ = lambda: load_spec(func, spec_path)
+        else:
+            func.__apispec__ = func.__dict__.get("__apispec__", {})
+            func.__apispec__.update(load_operations_from_docstring(content))
 
         return func
 
