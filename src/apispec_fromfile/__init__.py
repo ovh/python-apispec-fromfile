ee816b7cbaef2810aa840ee1d1cd46a1fac7d9c4
François Magimel <francois.magimel@ovhcloud.com>
2026-04-16 12:04:25 +0000

    build: extract the lib version dynamically
    The `pkg-resources` dependency is removed from `setuptools`. The version
    is now extracted via `setuptools-scm`.
    To get the version, feature detection should be used instead of
    `__version__`:
    ```
    importlib.metadata.version("apispec_fromfile")
    ```

--- src/apispec_fromfile/__init__.py
+++ src/apispec_fromfile/__init__.py
@@ -1,10 +1,6 @@
-import pkg_resources
-
 from .plugin import FromFilePlugin
 from .plugin import from_file
 
-
-__version__ = str(pkg_resources.get_distribution("apispec-fromfile").parsed_version)
 
 __all__ = [
     "FromFilePlugin",
